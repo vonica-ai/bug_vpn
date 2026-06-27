8cf33d343f06ac881916c87498fa8de7d32f080c
vonica-ai <ainothings59@gmail.com>
2026-06-27 08:21:19 +0630

    fix: add missing flutter/material.dart import for Color type

--- lib/app/shared/utils/utils.dart
+++ lib/app/shared/utils/utils.dart
@@ -1,6 +1,7 @@
-class AppUtils {
-  AppUtils._();
+import 'package:flutter/material.dart';
 
+class AppUtils
+  AppUtils._();
   static const String appLabel = 'Galaxy Tunnel';
   static const String appVersion = '1.0.0';
 
@@ -9,9 +10,5 @@ class AppUtils {
     final m = (seconds % 3600) ~/ 60;
     final s = seconds % 60;
     return '${h.toString().padLeft(2, '0')}:${m.toString().padLeft(2, '0')}:${s.toString().padLeft(2, '0')}';
-  }
-
-  static bool isDarkColor(Color color) {
-    return (color.red * 0.299 + color.green * 0.587 + color.blue * 0.114) < 128;
   }
 }
